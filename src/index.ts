507f669c2604a2592f609355476883fa247e1bc5
THONWELLING <thonwellingdani13@gmail.com>
2022-01-09 01:45:03 -0300

    commit inicial  configurando projeto node com typescript e express

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const PORT = 3333;
+
+app.get('/', (req, res) => {
+  res.send('Express + Typescript Server');
+});
+app.listen(PORT, () => {
+ 
+  return console.log(`Server is Running At http://localhost: ${PORT}`);
+});
